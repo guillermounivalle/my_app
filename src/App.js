967f317bc889982b0ea7876ea55d76af9ec3f649
Guillermo Hernandez <guillermo.hernandez@correounivalle.edu.co>
2018-09-23 00:55:41 -0500

    finished minitutorial React.js And firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import logo from './LogoTiger.png';
 
 import FileUpload from './FileUpload';
 import './App.css';
@@ -8,11 +9,13 @@ class App extends Component {
   constructor(){
     super(); // Si heredamos de otra clas, en este caso app hereda de Component
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     };
     //Hay que realizar un bindeo del "this"
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleOnUpload = this.handleOnUpload.bind(this);
   }
 
 
@@ -26,6 +29,13 @@ class App extends Component {
     firebase.auth().onAuthStateChanged(user =>{
       this.setState({user});
     })
+
+    //Listenes para la base de datos
+    firebase.database().ref('pictures').on('child_added', snapshot =>{
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
+    });
   }
 
   handleAuth(){
@@ -52,6 +62,42 @@ class App extends Component {
     .catch(error => console.log(`Error: ${error.code}: ${error.message}`));
   }
 
+  //Trabajamos con las opciones de firebase para subir imagenes
+  handleOnUpload(event){
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/Photos/${file.name}`);
+    const task = storageRef.put(file);
+    //const task = storageRef.child(`${file.name}`).put(file);
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalbytes) * 100;
+      this.setState({
+        uploadValue : percentage
+      })
+    }, error => { console.log(error.message) 
+    }, () => storageRef.getDownloadURL().then(url =>{
+      //Cuando se carga por completo entonces hace esto
+      // Subida completada
+      // Obtenemos la URL del fichero almacenado en Firebase storage
+      // Obtenemos la referencia a nuestra base de datos 'pictures'
+      // Creamos un nuevo registro en ella
+      // Guardamos la URL del enlace en la DB
+       
+       const record = {
+         photoURL: this.state.user.photoURL,
+         displayName: this.state.user.displayName,
+         image: url
+       };
+
+       const dbRef = firebase.database().ref('pictures');
+       const newPicture = dbRef.push();
+       console.log("PhotoUrl=====>"+record.photoURL+" Name ==> "+record.displayName+" url image ==>"+record.image);
+       newPicture.set(record);
+      }));
+  
+ }
+
+
 renderLoginButton(){
   /**
    * Entra la funcionalidad de react que son los estados. Si los estados son modificados, componente se va
@@ -60,38 +106,56 @@ renderLoginButton(){
    * 
    */
   //Si el usuario está logueado
-  if(this.state.user){
+  if (!this.state.user) {
     return (
-      <div>
-        <img src={this.state.user.photoURL} alt ={this.state.user.displayName} />
-        <p> Hola {this.state.user.displayName}</p>
-        <button onClick={this.handleLogout}>Logout</button>
-        <FileUpload/>
-      </div>  
-    ); 
-  }
-  else{
-    //Si no está logueado
-    return(
-      <button onClick= {this.handleAuth} > Login with Google</button>
+      <button onClick={this.handleAuth} className="App-btn">
+        Iniciar sesión con Google
+      </button>
     );
-  }
-
-  
-}
-
-  render() {
+  } else  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.renderLoginButton()} 
-        </p>
+      <div className="App-intro">
+        <p className="App-intro">¡Hola, { this.state.user.displayName }!</p>
+
+        <button onClick={this.handleLogout} className="App-btn">
+          Salir
+        </button>
+
+        <FileUpload onUpload={ this.handleOnUpload }/>
+
+        {
+          this.state.pictures.map(picture => (
+            <div className="App-card">
+              <figure className="App-card-image">
+                <img width="320" src={picture.image} />
+                <figCaption className="App-card-footer">
+                  <img className="App-card-avatar" src={picture.photoURL} alt={picture.displayName} />
+                  <span className="App-card-name">{picture.displayName}</span>
+                </figCaption>
+              </figure>
+            </div>
+          )).reverse()
+        }
+
       </div>
+
     );
   }
 }
+
+render() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>T3chfest 2017</h2>
+      </div>
+      { this.renderLoginButton() }
+    </div>
+  );
+}
+}
+
+ 
 
 export default App;
